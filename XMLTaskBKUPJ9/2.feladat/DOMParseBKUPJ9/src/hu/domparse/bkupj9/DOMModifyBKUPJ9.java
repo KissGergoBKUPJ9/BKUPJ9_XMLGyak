3cf6d026715d67d272222ec1a928f9774c867822
KissGergoBKUPJ9 <76010270+KissGergoBKUPJ9@users.noreply.github.com>
2021-12-07 14:18:11 +0100

    Add files via upload

---
+++ XMLTaskBKUPJ9/2.feladat/DOMParseBKUPJ9/src/hu/domparse/bkupj9/DOMModifyBKUPJ9.java
@@ -0,0 +1,48 @@
+package hu.domparse.bkupj9;
+
+import java.io.*;
+
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class DOMModifyBKUPJ9 {
+
+public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException { {
+        
+		String xmlFilePath = "XMLBKUPJ9.xml"; //File beolvasása
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new File(xmlFilePath));
+		
+		Node masodikMonitorozás = document.getElementsByTagName("monitorozás").item(1); //A 2. monitorozás elem gyerekelemeinek megkeresése
+		NodeList masodikMonitorozásNodeList = masodikMonitorozás.getChildNodes();
+		
+		for (int i = 0; i < masodikMonitorozásNodeList.getLength(); i++) { //A 2. monitorozás elem aktuális idájárásának megváltoztatása naposra
+			Node element = masodikMonitorozásNodeList.item(i);
+			if ("aktuálisIdájárás".equals(element.getNodeName())) {
+				element.setTextContent("napos");
+			}
+		}
+		
+		Element monitorozo = document.createElement("monitorozó"); //A 2. monitorozás elemhez hozzáadjuk a monitorozó nevût
+		monitorozo.appendChild(document.createTextNode("Béla"));
+		masodikMonitorozás.appendChild(monitorozo);
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance(); //A módosításokat feltöltjük az XML file-ba
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(document);
+		StreamResult result = new StreamResult(new File(xmlFilePath));
+		transformer.transform(source, result);
+		
+		System.out.println("Változtatások sikeresen eszközölve az XML file-on!!!!"); //Nyugtázzuk az elvégzett módosításokat
+		}
+	}
+}
---
+++ XMLTaskBKUPJ9/2.feladat/DOMParseBKUPJ9/src/hu/domparse/bkupj9/DOMQueryBKUPJ9.java
@@ -0,0 +1,57 @@
+package hu.domparse.bkupj9;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class DOMQueryBKUPJ9 {
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		
+		File file = new File("XMLBKUPJ9.xml");//Az XML file beolvasása
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();//DocumentBuilderFactory inicializálása
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();//A builder példonyosítása
+		 
+		Document doc = dBuilder.parse(file);//A dokumentum konvertálása hogy értse az xml-t
+		doc.getDocumentElement().normalize();
+		System.out.print("Gyökér element: ");//a gyökérelem kiíratása
+        System.out.println(doc.getDocumentElement().getNodeName());
+
+        NodeList nList = doc.getElementsByTagName("szenzorállomás");//szenzorállomások mentése listába
+        
+        
+        for (int i = 0; i < nList.getLength(); i++) {//szenzorállomás gyerek elemeinek megjelenítése a konzolon
+        	Node node = nList.item(i);
+            System.out.println("\nAktuális Elem : "+node.getNodeName());
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+            	Element elem = (Element) node;
+            	System.out.println("ID:"+elem.getAttribute("szenzorID"));
+            	NodeList nList2 = elem.getChildNodes();
+            	for (int j = 0; j < nList2.getLength(); j++) {
+            		Node node2 = nList2.item(j);
+					if (node2.getNodeType() == Node.ELEMENT_NODE) {
+						Element elem2 = (Element) node2;
+						NodeList nList3 = elem2.getChildNodes();
+						for (int k = 0; k < nList3.getLength(); k++) {
+							Node node3 = nList3.item(k);
+							if(node3.getNodeType()==Node.ELEMENT_NODE) {
+								Element elem3 = (Element) node3;
+								System.out.println(node3.getNodeName()+" : "+node3.getTextContent());
+							}
+						}
+					}
+				}
+            }
+		}
+	}
+}
